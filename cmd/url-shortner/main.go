f61ebade21a7b34e7eb6a3db077f1a217d98afce
sariya <dayzwaitik123@yandex.ru>
2024-08-29 09:41:18 +0300

    add redirect by alias

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
@@ -59,6 +60,7 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(ctx, log, storage))
+	router.Get("/{alias}", redirect.New(ctx, log, storage))
 
 	log.Info("starting server", "address", config.Address)
 	server := &http.Server{
---
+++ internal/http-server/handlers/redirect/redirect.go
@@ -0,0 +1,59 @@
+package redirect
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/xslog"
+	"url-shortener/internal/storage"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+type URLGetter interface {
+	GetURLByAlias(ctx context.Context, alias string) (string, error)
+}
+
+const (
+	ErrMsgGetURL = "failed to get URL"
+)
+
+func New(ctx context.Context, log *slog.Logger, getURL URLGetter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const operationPlace = "handlers.redirect.New"
+		log = log.With(
+			slog.String("op", operationPlace),
+			slog.String("requies_id", middleware.GetReqID(r.Context())),
+		)
+		alias := chi.URLParam(r, "alias")
+
+		if alias == "" {
+			log.Info("alias is empty")
+			render.JSON(w, r, response.Error("empty alias"))
+			return
+		}
+
+		url, err := getURL.GetURLByAlias(ctx, alias)
+
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("no url on this alias", "alias", alias)
+			render.JSON(w, r, response.Error(fmt.Sprintf("no url on this alias (alias=%s)", alias)))
+			return
+		}
+
+		if err != nil {
+			log.Error(ErrMsgGetURL, xslog.Err(err))
+			render.JSON(w, r, response.Error("internal error"))
+			return
+		}
+
+		log.Info("find url by alias", "alias", alias)
+		http.Redirect(w, r, url, http.StatusFound)
+
+	}
+}
