d2dfae13dceb61466e08f5ec8b68a5f4ae45fc98
sariya <dayzwaitik123@yandex.ru>
2024-08-29 15:55:52 +0300

    degative and positive tests for redirect

--- internal/http-server/handlers/redirect/redirect_test.go
+++ internal/http-server/handlers/redirect/redirect_test.go
@@ -2,31 +2,66 @@ package redirect_test
 
 import (
 	"context"
+	"encoding/json"
+	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/redirect/mocks"
 	"url-shortener/internal/lib/api"
+	"url-shortener/internal/lib/api/response"
 	"url-shortener/internal/lib/logger/handlers/slogpretty/slogdiscard"
+	"url-shortener/internal/storage"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRedirectFromAlias(t *testing.T) {
-	cases := []struct {
-		caseName  string
-		alias     string
-		url       string
-		respError string
-		mockError error
-	}{
+type testData struct {
+	caseName  string
+	alias     string
+	url       string
+	respError string
+	mockError error
+}
+
+func TestRedirectSuccess(t *testing.T) {
+	cases := []testData{
 		{
-			caseName: "Success redirect. Alias and URL in db",
+			caseName: "success redirect",
 			alias:    "zxc",
-			url:      "https://google.com",
+			url:      "http://google.com",
+		},
+	}
+	for _, testCase := range cases {
+		t.Run(testCase.caseName, func(t *testing.T) {
+			urlGetterMock := mocks.NewURLGetter(t)
+			ctx := context.Background()
+			urlGetterMock.On("GetURLByAlias", ctx, testCase.alias).Return(testCase.url, testCase.mockError).Once()
+			r := chi.NewRouter()
+			r.Get("/{alias}", redirect.New(ctx, slogdiscard.NewDiscardLogger(), urlGetterMock))
+			server := httptest.NewServer(r)
+			defer server.Close()
+
+			redirect, err := api.GetRedirect(server.URL + "/" + testCase.alias)
+			require.NoError(t, err)
+
+			assert.Equal(t, testCase.url, redirect)
+		})
+	}
+}
+
+func TestRedirectFromAlias(t *testing.T) {
+	cases := []testData{
+		{
+			caseName:  "No url on alias",
+			alias:     "qwe",
+			url:       "http://qwe.ru",
+			respError: "no url on this alias (alias=qwe)",
+			mockError: storage.ErrURLNotFound,
 		},
 	}
 
@@ -34,22 +69,22 @@ func TestRedirectFromAlias(t *testing.T) {
 		t.Run(testCase.caseName, func(t *testing.T) {
 			urlGetterMock := mocks.NewURLGetter(t)
 			ctx := context.Background()
-			if testCase.respError == "" || testCase.mockError != nil {
-				urlGetterMock.On("GetURLByAlias", ctx, testCase.alias).
-					Return(testCase.url, testCase.mockError).Once()
-			}
-
+			urlGetterMock.On("GetURLByAlias", ctx, testCase.alias).Return(testCase.url, testCase.mockError).Once()
 			r := chi.NewRouter()
 			r.Get("/{alias}", redirect.New(ctx, slogdiscard.NewDiscardLogger(), urlGetterMock))
-
 			server := httptest.NewServer(r)
-
 			defer server.Close()
+			client := http.Client{}
 
-			redirectedToURL, err := api.GetRedirect(server.URL + "/" + testCase.alias)
+			resp, err := client.Get(server.URL + "/" + testCase.alias)
+			require.NoError(t, err)
+			data, _ := io.ReadAll(resp.Body)
+			var res response.Response
+			err = json.Unmarshal(data, &res)
 			require.NoError(t, err)
 
-			assert.Equal(t, testCase.url, redirectedToURL)
+			assert.Equal(t, res.Error, testCase.respError)
+
 		})
 	}
 }
